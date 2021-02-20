534a312580c0a28405947ce52e3c02db40e76ea2
velllum <velllum@ya.ru>
2021-02-20 16:44:20 +0300

    commit 2

--- parse.py
+++ parse.py
@@ -1,10 +1,13 @@
 import os
+from telnetlib import EC
 
 from selenium import webdriver
 import time
 
 from fake_useragent import UserAgent
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
 
 user_agent = UserAgent()
 base_path = os.path.abspath(os.path.dirname(__file__))
@@ -15,13 +18,14 @@ options = webdriver.ChromeOptions()
 
 options.add_argument(f"user-agent={user_agent.random}")
 options.add_argument("--disable-blink-features=AutomationControlled")
-options.add_argument("--start-maximized")
+# options.add_argument("--start-maximized")
+options.add_argument("--headless")
 options.add_experimental_option('prefs',  {
         "download.default_directory": file_path,
         "download.prompt_for_download": False,
         "download.directory_upgrade": True,
-        # "plugins.always_open_pdf_externally": True,
-        "plugins.plugins_disabled": ["Chrome PDF Viewer"]
+        "plugins.always_open_pdf_externally": True,
+        # "plugins.plugins_disabled": ["Chrome PDF Viewer"]
 
     }
 )
@@ -57,11 +61,15 @@ try:
     driver.get("https://stomshop.pro/woodpecker-led-d")
     driver.find_element_by_id("tab-documentation-li").click()
     time.sleep(1)
+    print(driver.find_element_by_tag_name('a').get_attribute('href'))  # "innerHTML"
+    print(driver.find_element_by_class_name('documentation-download').get_attribute("innerHTML"))  # "innerHTML"
     driver.find_element_by_class_name("docext-container").click()
-    time.sleep(3)
+    time.sleep(1)
     # driver.find_element("text-decoration:none;").click()
-    driver.find_element_by_xpath("//i[@class='fa-download']").click()
-    time.sleep(100)
+    # driver.find_element_by_xpath("//i[@class='fa-download']").click()
+
+
+    driver.find_element_by_xpath('//a[@style="text-decoration:none;"]').get_attribute('href')
 
 
 except Exception as ex:
---
+++ test_requests.py
@@ -0,0 +1,7 @@
+import requests
+
+response = requests.post('https%3A%2F%2Fstomshop.pro%2Fwoodpecker-led-d&pointer-click=rn%3A831498509%3Ax%3A35243%3Ay%3A52428%3At%3A18460%3Ap%3A%3B%5D1b%5C%5BAA5A1AA1AAA%3AX%3A1410%3AY%3A1054&browser-info=gdpr%3A14%3Au%3A1613740006536152535%3Av%3A421%3Avf%3Acaxsklyqnpvsij3%3Arqnl%3A1%3Ati%3A0%3Ast%3A1613810670&force-urlencoded=1')
+
+
+
+print(response.text)
